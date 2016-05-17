6b37ad1b2f9fc2c15f57fcdabf7f8975c91ba61f
Mitar <mitar.git@tnode.com>
2016-05-17 11:56:13 -0400

    New test.

--- test.js
+++ test.js
@@ -29,14 +29,23 @@ function generateKey() {
   });
 }
 
-Promise.all([generateKey(), generateKey()]).then(function (keys) {
+function publicKey() {
+  var content = new Buffer('3059301306072a8648ce3d020106082a8648ce3d030107034200047a9f5058422c18043bcb3df3ea56b513a533cfe4c2c3fa489a8606bff6a6677d9a9eb0505fcc8011a329013f96b3646a438ea4e55343a0d3b8fdf60c954c36e5', 'hex');
+
+  return crypto.subtle.importKey('spki', content, {
+    name: 'ECDH',
+    namedCurve: 'P-256'
+  }, false, [])
+}
+
+Promise.all([publicKey(), generateKey()]).then(function (keys) {
   var keyPairA = keys[0];
   var keyPairB = keys[1];
-  
+
   return crypto.subtle.deriveBits({
     name: 'ECDH',
     namedCurve: 'P-256',
-    public: keyPairA[PUBLIC_KEY]
+    public: keyPairA
   }, keyPairB[PRIVATE_KEY], 256);
 }).then(function (bits) {
   return crypto.subtle.digest({
@@ -44,4 +53,6 @@ Promise.all([generateKey(), generateKey()]).then(function (keys) {
   }, bits);
 }).then(function (hashedBits) {
   console.log("hashedBits", new Buffer(hashedBits).toString('hex'));
+}).catch(function (error) {
+  console.log("error", error);
 });
